b6fa80445c71783d21c43269cc72e26e07e9d3b6
dmabelin <d.mabelin@arcanys.com>
2021-09-14 15:07:20 +0800

    fix package issue

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ setup(
     author_email='thedjaney@gmail.com',
     description='',
     install_requires=[
-        'tasktimer',
+        'djaneytasktimer',
         'pyqt5==5.14.*',
     ],
     dependency_links=[
-        'https://github.com/djaney/tasktimer/archive/refs/heads/main.zip#egg=tasktimer'
+        'https://github.com/djaney/tasktimer/archive/refs/heads/main.zip#egg=djaneytasktimer'
     ],
     entry_points={
         "gui_scripts": [
