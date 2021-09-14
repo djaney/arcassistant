a2ca14a4e5eaa59a65eb135e69e3d0ce63f1670f
dmabelin <d.mabelin@arcanys.com>
2021-09-14 14:34:50 +0800

    find packaghes

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='arcassistant',
     version='0.0.0',
-    packages=['arcassistant'],
+    packages=find_packages(),
     url='',
     license='',
     author='thedjaney',
