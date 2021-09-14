7c538001e04bd39220bcf43c9a4a40d57983dc58
dmabelin <d.mabelin@arcanys.com>
2021-09-14 14:42:21 +0800

    fix package issue

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='arcassistant',
     version='0.0.0',
-    packages=find_packages('arcassistant'),
+    packages=find_packages(),
     url='',
     license='',
     author='thedjaney',
