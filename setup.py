ddbf32fa383bd765cb93db42d4b03fc261b54266
Djane Rey Mabelin <djaney@users.noreply.github.com>
2022-04-27 13:28:52 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='arcassistant',
-    version='0.0.0',
+    version='1.0.0',
     packages=find_packages(),
     url='',
     license='',
